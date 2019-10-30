fd4a9bea057a61a23ced1983355cf52b187c3d3a
guochengli1997 <44562174+guochengli1997@users.noreply.github.com>
2019-10-30 15:39:53 +0800

    添加循环输出

--- githubTest/src/com/yunhe/test/Print.java
+++ githubTest/src/com/yunhe/test/Print.java
@@ -3,5 +3,8 @@ package com.yunhe.test;
 public class Print {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
 	}
 }
